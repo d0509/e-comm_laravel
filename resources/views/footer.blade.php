9b692d55b18b7459371f8aecfb990ff4eed0e973
d0509 <dattap0509@gmail.com>
2023-06-24 10:31:15 +0530

    migration and seeder part

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,11 +1,12 @@
-<div class="card">
-  <div class="card-header">
-    Quote
-  </div>
-  <div class="card-body">
-    <blockquote class="blockquote mb-0">
-      <p>A well-known quote, contained in a blockquote element.</p>
-      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-    </blockquote>
-  </div>
-</div>
+
+    <div class="card col-12" style="position:absolute;bottom:10px;">
+        <div class="card-header">
+            Quote
+        </div>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>A well-known quote, contained in a blockquote element.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+            </blockquote>
+        </div>
+    </div>
